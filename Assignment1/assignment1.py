c1d26d871dd2dfbc5135e17954bf43b1fb727e26
VincentT <59984283+Vincent-Talen@users.noreply.github.com>
2023-05-25 11:20:36 +0200

    Added argparser supplied by course

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -8,14 +8,24 @@ Usage:
     $ python3 assignment1.py -n <cpu_count> [-o <output csv file>] fastq_file1.fastq [fastq_file2.fastq ... fastq_fileN.fastq]
 """
 
+# METADATA
 __author__ = "Vincent Talen"
 __version__ = "0.0"
 
-# Imports
+# IMPORTS
+import argparse as ap
+
 
 # FUNCTIONS
 def main():
-    ...
+    argparser = ap.ArgumentParser(description="Script voor Opdracht 1 van Big Data Computing")
+    argparser.add_argument("-n", action="store",
+                           dest="n", required=True, type=int,
+                           help="Aantal cores om te gebruiken.")
+    argparser.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'),
+                           required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
+    argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+', help="Minstens 1 Illumina Fastq Format file om te verwerken")
+    args = argparser.parse_args()
 
 
 if __name__ == "__main__":
